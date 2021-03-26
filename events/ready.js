1c34cc26d78d3b8510647a29e17fdb273752ca32
Ari-721 <69311965+Ari-721@users.noreply.github.com>
2021-03-26 18:42:32 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("?help | ${client.guilds.cache.size} Server | Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} |", {
+  await client.user.setActivity("| ?help | ", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
