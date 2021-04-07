d65de257f94e21454639fca764a88838acbf418b
Ari-721 <69311965+Ari-721@users.noreply.github.com>
2021-04-07 19:58:37 +0300

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -13,7 +13,7 @@ module.exports = {
     //set the permissions id here (https://discordapi.com/permissions.html)
     var permissions = 37080128;
     
-    let invite = new MessageEmbed()
+    let owner = new MessageEmbed()
     .setTitle(`owner ${client.user.username}`)
     .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
     .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
