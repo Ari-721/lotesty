ff5560f926ebc183fe70e7a99cfa74bcdc682342
Ari-721 <69311965+Ari-721@users.noreply.github.com>
2021-04-14 02:01:54 +0300

    Create bot.js

---
+++ commands/bot.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  info: {
+    name: "bot",
+    description: "Bot info",
+    usage: "[bot]",
+    aliases: ["bot"],
+  },
+
+  run: async function (client, message, args) {
+    
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 37080128;
+    
+    let bot = new MessageEmbed()
+    .setTitle(`Support Server ${client.user.username}`)
+    .setDescription(`
+    
+    Name : Bolt Music
+    Servers  : ${client.guilds.cache.size}
+    Users    : ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}
+    Channels : ${client.channels.cache.size}
+    Prefix   : (?)
+    Bot Owner : <@363268408915132418>
+    
+    `)
+    .setURL(`https://discord.gg/v7KNdSv `)
+    .setColor("BLUE")
+    return message.channel.send(bot);
+  },
+};
