7188ba5c5f04fb08f6f14d8d2d5fd2b56c66c909
Ari-721 <69311965+Ari-721@users.noreply.github.com>
2021-04-13 20:20:08 +0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -17,9 +17,9 @@ module.exports = {
     if (!serverQueue.connection) return sendError("There is nothing playing in this server.", message.channel);
     if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
      if(isNaN(args[0])) return message.channel.send(':notes: Numbers only!').catch(err => console.log(err));
-    if(parseInt(args[0]) > 200 ||(args[0]) < 0) return sendError('You can\'t set the volume more than 200. or lower than 0',message.channel).catch(err => console.log(err));
+    if(parseInt(args[0]) > 300 ||(args[0]) < 0) return sendError('You can\'t set the volume more than 300. or lower than 0',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
-    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 300);
     let xd = new MessageEmbed()
     .setDescription(`I set the volume to: **${args[0]/1}/100**`)
     .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
